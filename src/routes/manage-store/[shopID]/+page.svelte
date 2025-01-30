784cd1dce27c1eac17727782d857634365ed1a7e
Idea RC <ideapakpaphon@gmail.com>
2025-01-31 01:51:29 +0700

    Manage Store Features

--- src/routes/manage-store/[shopID]/+page.svelte
+++ src/routes/manage-store/[shopID]/+page.svelte
@@ -140,6 +140,67 @@
 </section>
 
 
+<section id="stats" class="flex w-full items-center justify-center">
+    <div class="stats shadow">
+        <div class="stat">
+          <div class="stat-figure text-secondary">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              class="inline-block h-8 w-8 stroke-current">
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+            </svg>
+          </div>
+          <div class="stat-title">ผู้ชมร้านค้า</div>
+          <div class="stat-value">0</div>
+          <div class="stat-desc">Jan 1st - Feb 1st</div>
+        </div>
+      
+        <div class="stat">
+          <div class="stat-figure text-secondary">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              class="inline-block h-8 w-8 stroke-current">
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"></path>
+            </svg>
+          </div>
+          <div class="stat-title">ผู้ติดตามร้านค้า</div>
+          <div class="stat-value">0</div>
+          <div class="stat-desc">↗︎ 400 (22%)</div>
+        </div>
+      
+        <div class="stat">
+          <div class="stat-figure text-secondary">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              class="inline-block h-8 w-8 stroke-current">
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"></path>
+            </svg>
+          </div>
+          <div class="stat-title">จำนวนผู้ซื้อสินค้า</div>
+          <div class="stat-value">0</div>
+          <div class="stat-desc">0</div>
+        </div>
+      </div>
+
+</section>
 
 
 
@@ -149,7 +210,7 @@
         <section class="mb-6">
             <h2 class="mb-2 text-2xl font-semibold">จัดการสินค้า</h2>
         </section>
-        <button class="btn btn-primary mb-4" on:click={() => my_modal_5.showModal()}>+ เพิ่มสินค้า</button>
+        <button class="btn btn-outline mb-4" on:click={() => my_modal_5.showModal()}>+ เพิ่มสินค้า</button>
         
         <dialog id="my_modal_5" class="modal modal-bottom sm:modal-middle">
         <div class="modal-box">
